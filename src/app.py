19c40b7c115d15a82fabb2199678f550959d8ab0
José Borges <110873863+BorgesJos@users.noreply.github.com>
2026-03-17 00:21:45 +0000

    API FAMILY

--- src/app.py
+++ src/app.py
@@ -28,14 +28,87 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-
+# GET: Mostrar Miembros de la familia
 @app.route('/members', methods=['GET'])
 def handle_hello():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
+
+
+# POST: Crear nuevo miembro
+@app.route('/members', methods=['POST'])
+def add_member():
+    new_member = request.json
+    jackson_family.add_member(new_member)
+
+    return jsonify(new_member), 200
+
+
+# GET: Obtener un miembro de la familia especifico
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    find_member = jackson_family.get_member(id)
+    if not find_member:
+        return jsonify({"msg": "No se encontro al miembro"}), 400
+    return jsonify(find_member), 200
+
+
+# # PUT: Actualizar miembro de la familia
+# @app.route('/member/<int:member_id>', methods=['PUT'])
+# def actualizar_member(member_id):
+#     new_member = request.json()
+#     update_member = jackson_family.update_member(member_id, new_member)
+#     if not update_member:
+#         return jsonify({"msg": "No se encontro al miembro"}), 400
+#     return jsonify({"DONE!!": "Familiar Actualizado"}), 200
+
+
+# DELETE: Borrar miembro de la familia
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    # This is how you can use the Family datastructure by calling its methods
+    eliminar_f = jackson_family.delete_member(id)
+    if not eliminar_f:
+        return jsonify({"msg": "Familiar no encontrado"}), 400
+    
+    return jsonify({"done": True}), 200
+
+# GET: Obtener un miembro de la familia especifico
+# @app.route('/member/<int:member_id>', methods=['GET'])
+# def get_member(member_id):
+#     find_member = jackson_family.get_member(member_id)
+#     if not find_member:
+#         return jsonify({"msg": "No se encontro al miembro"}), 400
+#     return jsonify(find_member), 200
+
+
+# @app.route('/member/<int:member_id>', methods=['PUT'])
+# def actualizar_member(member_id):
+#     new_member = request.json()
+#     update_member = jackson_family.update_member(member_id, new_member)
+#     if not update_member:
+#         return jsonify({"msg": "No se encontro al miembro"}), 400
+#     return jsonify({"DONE!!": "Familiar Actualizado"}), 200
+
+
+# @app.route('/member/<int:member_id>', methods=['GET'])
+# def get_member(member_id):
+#     find_member = jackson_family.get_member(member_id)
+#     if not find_member:
+#         return jsonify({"msg": "No se encontro al miembro"}), 400
+#     return jsonify(find_member), 200
+
+
+
+
+# @app.route('/nuevo', methods=['GET'])
+# def add_member():
+#     # This is how you can use the Family datastructure by calling its methods
+#     members = jackson_family.get_all_members()
+#     response_body = {"hello": "world",
+#                      "family": members}
+#     return jsonify(response_body), 200
 
 
 
--- src/datastructures.py
+++ src/datastructures.py
@@ -16,8 +16,22 @@ class FamilyStructure:
                 "last_name": last_name,
                 "age": 33,
                 "lucky_numbers": [7, 13, 22]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jane",
+                "last_name": last_name,
+                "age": 35,
+                "lucky_numbers": [10, 14, 3]
+            },
+            {
+                "id": self._generate_id(),
+                "first_name": "Jimmy",
+                "last_name": last_name,
+                "age": 5,
+                "lucky_numbers": [1]
             }
-        ]
+]
 
     # This method generates a unique incremental ID
     def _generate_id(self):
@@ -26,19 +40,43 @@ class FamilyStructure:
         return generated_id
 
     def add_member(self, member):
+        
         ## You have to implement this method
         ## Append the member to the list of _members
-        pass
+        if not member.get("id"):
+          member["id"] = self._generate_id()
+        self._members.append(member)
+
 
     def delete_member(self, id):
         ## You have to implement this method
         ## Loop the list and delete the member with the given id
-        pass
+        print(id)
+        for member in self._members:
+            if member["id"] == id:
+                self._members.remove(member)
+                return True
+        return False
+    
+    def update_member(self, member, id):
+        ## You have to implement this method
+        ## Loop all the members and return the one with the given id
+        print("Actualizando", id)
+        for famimy_member in self._members:
+            if famimy_member["id"] == id:
+                self._members.remove(famimy_member)
+                member["id"] = id
+                self._members.append(member)
+                return True   
+        return False   
 
     def get_member(self, id):
         ## You have to implement this method
         ## Loop all the members and return the one with the given id
-        pass
+        for family_member in self._members:
+            if family_member["id"] == id:
+                return family_member
+        return False
 
     # This method is done, it returns a list with all the family members
     def get_all_members(self):
